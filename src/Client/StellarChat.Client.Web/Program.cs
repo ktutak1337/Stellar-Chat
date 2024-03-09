f1cd3d9a6c0e40d96f1c064eb870aa6260f7c876
ktutak1337 <ktutak1337@gmail.com>
2024-03-09 07:31:01 +0100

    Setup(UI::MudBlazor): Integrate MudBlazor into the project

--- src/Client/StellarChat.Client.Web/Program.cs
+++ src/Client/StellarChat.Client.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using StellarChat.Client.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
