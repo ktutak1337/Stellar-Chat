e9a6e75fcc25c25f8b52d44450563aa6884dd181
ktutak1337 <ktutak1337@gmail.com>
2024-03-26 05:54:08 +0100

    Code(API::ChatSession): Add ChatSession model

---
+++ src/Server/StellarChat.Server.Api/Models/Chat/ChatSession.cs
@@ -0,0 +1,24 @@
+﻿namespace StellarChat.Server.Api.Models.Chat;
+
+public class ChatSession
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Metaprompt { get; set; }
+    public HashSet<string> ActivePlugins { get; set; } = new();
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
+
+    public ChatSession(Guid id, string title, string metaprompt, HashSet<string> activePlugins, DateTimeOffset createdAt, DateTimeOffset updatedAt)
+    {
+        Id = id;
+        Title = title;
+        Metaprompt = metaprompt;
+        ActivePlugins = activePlugins;
+        CreatedAt = createdAt;
+        UpdatedAt = updatedAt;
+    }
+
+    public static ChatSession Create(Guid id, string title, string metaprompt, HashSet<string> activePlugins, DateTimeOffset createdAt, DateTimeOffset updatedAt)
+        => new(id, title, metaprompt, activePlugins, createdAt, updatedAt);
+}
