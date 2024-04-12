6f0814d7c8509239836f47180b3e416df4ec3fe5
ktutak1337 <ktutak1337@gmail.com>
2024-04-13 01:28:34 +0200

    Code(API::Exceptions): Add exceptions for chat message

---
+++ src/Server/StellarChat.Server.Api/DAL/Mongo/Exceptions/Chat/ChatMessageAlreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿namespace StellarChat.Server.Api.DAL.Mongo.Exceptions.Chat;
+
+public class ChatMessageAlreadyExistsException : StellarChatException
+{
+    public Guid Id { get; }
+
+    public ChatMessageAlreadyExistsException(Guid id)
+        : base(
+            message: $"Chat message with Id: '{id}' already exists.",
+            userMessage: $"A chat message with the same details already exists.")
+        => Id = id;
+}
---
+++ src/Server/StellarChat.Server.Api/DAL/Mongo/Exceptions/Chat/ChatMessageNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace StellarChat.Server.Api.DAL.Mongo.Exceptions.Chat;
+
+public class ChatMessageNotFoundException : StellarChatException
+{
+    public Guid Id { get; }
+
+    public ChatMessageNotFoundException(Guid id)
+        : base(
+            message: $"Chat message with ID: {id} not found.",
+            userMessage: $"The requested chat message could not be found.")
+        => Id = id;
+}
