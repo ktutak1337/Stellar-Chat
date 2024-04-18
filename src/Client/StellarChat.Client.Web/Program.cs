5a0388efaae6310341ee7c3dc4f304650b472439
ktutak1337 <ktutak1337@gmail.com>
2024-04-18 06:15:15 +0200

    Code(WEB::HttpClient): Configure HttpClient for WebAPI integration

--- src/Client/StellarChat.Client.Web/Program.cs
+++ src/Client/StellarChat.Client.Web/Program.cs
@@ -7,7 +7,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("WebAPI", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7057");
+});
+
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
