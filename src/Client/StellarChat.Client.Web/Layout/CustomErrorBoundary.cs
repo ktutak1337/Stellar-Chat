1c8f6880b050876b206e64962260f82a129181e9
ktutak1337 <ktutak1337@gmail.com>
2024-05-01 20:36:26 +0200

    Code(WEB::ErrorHandling): Implement global error handling

---
+++ src/Client/StellarChat.Client.Web/Layout/CustomErrorBoundary.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace StellarChat.Client.Web.Layout;
+
+public class CustomErrorBoundary : ErrorBoundary
+{
+    [Inject]
+    private IWebAssemblyHostEnvironment? Environment { get; set; }
+
+    protected override Task OnErrorAsync(Exception exception)
+    {
+        if (Environment!.IsDevelopment())
+        {
+            return base.OnErrorAsync(exception);
+        }
+
+        return Task.CompletedTask;
+    }
+}
