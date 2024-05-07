027f9056a2227ac267df6159358af73bacaf3363
ktutak1337 <ktutak1337@gmail.com>
2024-05-08 01:33:06 +0200

    Code(Shared::Contract): Add shared contract for AssistantResponse

---
+++ src/Shared/StellarChat.Shared.Contracts/Assistants/AssistantResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace StellarChat.Shared.Contracts.Assistants;
+
+public record AssistantResponse(
+    Guid Id,
+    string Name,
+    string Metaprompt, 
+    string Description,
+    string AvatarUrl,
+    string DefaultModel,
+    string DefaultVoice,
+    bool IsDefault,
+    DateTimeOffset CreatedAt,
+    DateTimeOffset UpdatedAt);
