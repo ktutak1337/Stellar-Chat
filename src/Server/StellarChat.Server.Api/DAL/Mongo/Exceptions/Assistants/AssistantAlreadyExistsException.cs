f872a49fc88255e62852dc6bd998a8ee5055f129
ktutak1337 <ktutak1337@gmail.com>
2024-05-08 01:32:37 +0200

    Code(API::DAL): Add custom exceptions for Assistant entities

---
+++ src/Server/StellarChat.Server.Api/DAL/Mongo/Exceptions/Assistants/AssistantAlreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿namespace StellarChat.Server.Api.DAL.Mongo.Exceptions.Assistants;
+
+public class AssistantAlreadyExistsException : StellarChatException
+{
+    public Guid Id { get; }
+
+    public AssistantAlreadyExistsException(Guid id)
+        : base(
+            message: $"Assistant with Id: '{id}' already exists.",
+            userMessage: $"An assistant with the same details already exists.")
+        => Id = id;
+}
---
+++ src/Server/StellarChat.Server.Api/DAL/Mongo/Exceptions/Assistants/AssistantNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace StellarChat.Server.Api.DAL.Mongo.Exceptions.Assistants;
+
+public class AssistantNotFoundException : StellarChatException
+{
+    public Guid Id { get; }
+
+    public AssistantNotFoundException(Guid id)
+        : base(
+            message: $"Assistant message with ID: {id} not found.",
+            userMessage: $"The requested assistant could not be found.")
+        => Id = id;
+}
+
