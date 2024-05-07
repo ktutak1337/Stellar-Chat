33b10020239a6c1e362912ae778d8684c6885673
ktutak1337 <ktutak1337@gmail.com>
2024-05-08 00:38:37 +0200

    Code(API::Domain): Add IAssistantRepository interface for Assistant entity management

---
+++ src/Server/StellarChat.Server.Api/Domain/Assistants/Repositories/IAssistantRepository.cs
@@ -0,0 +1,13 @@
+﻿using StellarChat.Server.Api.Domain.Assistants.Models;
+
+namespace StellarChat.Server.Api.Domain.Assistants.Repositories;
+
+internal interface IAssistantRepository
+{
+    ValueTask<Assistant?> GetAsync(Guid id);
+    ValueTask<IEnumerable<Assistant>> BrowseAsync();
+    ValueTask AddAsync(Assistant assistant);
+    ValueTask UpdateAsync(Assistant assistant);
+    ValueTask DeleteAsync(Guid id);
+    ValueTask<bool> ExistsAsync(Guid id);
+}
