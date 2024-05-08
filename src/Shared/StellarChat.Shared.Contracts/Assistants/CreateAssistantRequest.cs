8820eca717ca7d9e53da7b987f4ae7c6202f3256
ktutak1337 <ktutak1337@gmail.com>
2024-05-08 03:54:57 +0200

    Code(Shared::Contract): Add shared contract for CreateAssistantRequest

---
+++ src/Shared/StellarChat.Shared.Contracts/Assistants/CreateAssistantRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StellarChat.Shared.Contracts.Assistants;
+
+public record CreateAssistantRequest(
+    [property: JsonIgnore] Guid Id,
+    string Name,
+    string Metaprompt,
+    string Description,
+    string AvatarUrl,
+    string DefaultModel,
+    string DefaultVoice,
+    bool IsDefault);
