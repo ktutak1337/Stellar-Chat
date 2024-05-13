5bc56fe7b5dbf298affebe97f67bcc64efb7ef4d
ktutak1337 <ktutak1337@gmail.com>
2024-05-14 01:50:14 +0200

    Code(WEB::ChatState): Implement global state to manage chat state

--- src/Client/StellarChat.Client.Web/Program.cs
+++ src/Client/StellarChat.Client.Web/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using StellarChat.Client.Web;
 using StellarChat.Client.Web.Services.Chat;
+using StellarChat.Client.Web.State;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -13,6 +14,7 @@ builder.Services.AddHttpClient("WebAPI", client =>
     client.BaseAddress = new Uri("https://localhost:7057");
 });
 
+builder.Services.AddScoped<ChatState>();
 builder.Services.AddMudServices();
 builder.Services.AddScoped<IChatService, ChatService>();
 
---
+++ src/Client/StellarChat.Client.Web/State/ChatState.cs
@@ -0,0 +1,13 @@
+﻿namespace StellarChat.Client.Web.State;
+
+public class ChatState
+{
+    public Guid ChatId { get; set; }
+    public event Action? ChatIdChanged;
+
+    public void SetChatId(Guid chatId)
+    {
+        ChatId = chatId;
+        ChatIdChanged?.Invoke();
+    }
+}
