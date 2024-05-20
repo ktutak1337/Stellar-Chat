4ec90a315aa300396de118d4550b74124eb9cd65
ktutak1337 <ktutak1337@gmail.com>
2024-05-20 08:47:35 +0200

    Code(WEB::AssistantService): Implement fetching assistants from API for UI display

---
+++ src/Client/StellarChat.Client.Web/Services/Assistants/AssistantService.cs
@@ -0,0 +1,22 @@
+﻿using StellarChat.Client.Web.Shared;
+using StellarChat.Shared.Contracts.Assistants;
+using System.Net.Http.Json;
+
+namespace StellarChat.Client.Web.Services.Assistants;
+
+public class AssistantService : IAssistantService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public AssistantService(IHttpClientFactory httpClientFactory) 
+        => _httpClientFactory = httpClientFactory;
+
+    public async ValueTask<Paged<AssistantResponse>> BrowseAssistants(int page = 0, int pageSize = 10000)
+    {
+        var httpClient = _httpClientFactory.CreateClient("WebAPI");
+
+        var result = await httpClient.GetFromJsonAsync<Paged<AssistantResponse>>($"/assistants?Page={page}&PageSize={pageSize}");
+
+        return result!;
+    }
+}
---
+++ src/Client/StellarChat.Client.Web/Services/Assistants/IAssistantService.cs
@@ -0,0 +1,9 @@
+﻿using StellarChat.Client.Web.Shared;
+using StellarChat.Shared.Contracts.Assistants;
+
+namespace StellarChat.Client.Web.Services.Assistants;
+
+public interface IAssistantService
+{
+    ValueTask<Paged<AssistantResponse>> BrowseAssistants(int page = 0, int pageSize = 10000);
+}
