f5b0357f8ee82545b185503981c98e25d3b40cb6
ktutak1337 <ktutak1337@gmail.com>
2024-05-23 12:12:17 +0200

    Code(WEB::AssistantForm): Add validation for AssistantForm

--- src/Client/StellarChat.Client.Web/Models/Assistant.cs
+++ src/Client/StellarChat.Client.Web/Models/Assistant.cs
@@ -1,13 +1,28 @@
-﻿namespace StellarChat.Client.Web.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StellarChat.Client.Web.Models;
 
 public class Assistant
 {
     public Guid Id { get; set; }
+    
+    [Required]
+    [MinLength(2, ErrorMessage = "Name must be at least 2 characters long.")]
     public string Name { get; set; } = string.Empty;
+    
+    [Required] 
     public string Metaprompt { get; set; } = string.Empty;
+    
+    [Required]
+    [MinLength(5, ErrorMessage = "Name must be at least 5 characters long.")]
     public string Description { get; set; } = string.Empty;
+    
+    [Required]
     public string AvatarUrl { get; set; } = string.Empty;
+
+    [Required] 
     public string DefaultModel { get; set; } = string.Empty;
+
     public string DefaultVoice { get; set; } = string.Empty;
     public bool IsDefault { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
