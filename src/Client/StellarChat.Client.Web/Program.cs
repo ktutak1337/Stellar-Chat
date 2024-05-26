0eb6ac3df41892f1d149aa7a03170fbf9ad01304
ktutak1337 <ktutak1337@gmail.com>
2024-05-26 12:10:40 +0200

    Code(WEB::ChatHistory): Add chat messages with text streaming support

--- src/Client/StellarChat.Client.Web/Program.cs
+++ src/Client/StellarChat.Client.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using MudBlazor.Services;
 using StellarChat.Client.Web;
 using StellarChat.Client.Web.Services.Assistants;
@@ -9,6 +10,7 @@ using StellarChat.Client.Web.State;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.TryAddSingleton(TimeProvider.System);
 
 builder.Services.AddHttpClient("WebAPI", client =>
 {
--- src/Shared/StellarChat.Shared.Contracts/Chat/ChatMessageResponse.cs
+++ src/Shared/StellarChat.Shared.Contracts/Chat/ChatMessageResponse.cs
@@ -1,3 +1,11 @@
 ﻿namespace StellarChat.Shared.Contracts.Chat;
 
-public record ChatMessageResponse(Guid Id, Guid ChatId, ChatMessageType Type, string Author, string Content, DateTimeOffset Timestamp);
+public class ChatMessageResponse
+{
+    public Guid Id { get; set; }
+    public Guid ChatId { get; set; }
+    public ChatMessageType Type { get; set; }
+    public string Author { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTimeOffset Timestamp { get; set; }
+}
