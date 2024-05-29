64540cec0015c1011db1aa03717fc00bd77e6b27
ktutak1337 <ktutak1337@gmail.com>
2024-05-29 08:52:12 +0200

    Code(API::DAL): Add exceptions for actions

---
+++ src/Server/StellarChat.Server.Api/DAL/Mongo/Exceptions/Actions/NativeActionAlreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿namespace StellarChat.Server.Api.DAL.Mongo.Exceptions.Actions;
+
+public class NativeActionAlreadyExistsException : StellarChatException
+{
+    public Guid Id { get; }
+
+    public NativeActionAlreadyExistsException(Guid id)
+        : base(
+            message: $"Action with Id: '{id}' already exists.",
+            userMessage: $"A similar action already exists.")
+        => Id = id;
+}
---
+++ src/Server/StellarChat.Server.Api/DAL/Mongo/Exceptions/Actions/NativeActionNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace StellarChat.Server.Api.DAL.Mongo.Exceptions.Actions;
+
+public class NativeActionNotFoundException : StellarChatException
+{
+    public Guid Id { get; }
+
+    public NativeActionNotFoundException(Guid id)
+        : base(
+            message: $"Action with ID: {id} not found.",
+            userMessage: $"The requested action could not be found.")
+        => Id = id;
+}
