c57b771f24a501a180169aad9a55d5c3ea3d75d2
ktutak1337 <ktutak1337@gmail.com>
2024-05-29 06:23:06 +0200

    Code(API::Domain): Add NativeAction model

---
+++ src/Server/StellarChat.Server.Api/Domain/Actions/Models/NativeAction.cs
@@ -0,0 +1,53 @@
+﻿namespace StellarChat.Server.Api.Domain.Actions.Models;
+
+internal class NativeAction
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public string Icon { get; set; }
+    public string Model { get; set; }
+    public string Metaprompt { get; set; }
+    public bool IsRemoteAction { get; set; }
+    public bool ShouldRephraseResponse { get; set; }
+    public Webhook? Webhook { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
+    
+    public NativeAction(
+        Guid id, 
+        string name, 
+        string category, 
+        string icon, 
+        string model, 
+        string metaprompt, 
+        bool isRemoteAction, 
+        Webhook? webhook, 
+        DateTimeOffset createdAt, 
+        DateTimeOffset updatedAt)
+    {
+        Id = id;
+        Name = name;
+        Category = category;
+        Icon = icon;
+        Model = model;
+        Metaprompt = metaprompt;
+        IsRemoteAction = isRemoteAction;
+        Webhook = webhook;
+        CreatedAt = createdAt;
+        UpdatedAt = updatedAt;
+    }
+
+    public static NativeAction Create(
+        Guid id,
+        string name,
+        string category,
+        string icon,
+        string model,
+        string metaprompt,
+        bool isRemoteAction,
+        Webhook? webhook,
+        DateTimeOffset createdAt,
+        DateTimeOffset updatedAt)
+            => new(id, name, category, icon, model, metaprompt, isRemoteAction, webhook, createdAt, updatedAt);
+}
