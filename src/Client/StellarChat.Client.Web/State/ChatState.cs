e14756086c6da16b0a96299818ab0d149613bcb3
ktutak1337 <ktutak1337@gmail.com>
2024-06-02 11:45:18 +0200

    Code(WEB::ProfileForm): Store user profile data in ChatState

--- src/Client/StellarChat.Client.Web/State/ChatState.cs
+++ src/Client/StellarChat.Client.Web/State/ChatState.cs
@@ -16,4 +16,22 @@ public class ChatState
         ChatId = Guid.Empty;
         ChatIdChanged?.Invoke();
     }
+
+    public string UserName { get; set; } = string.Empty;
+    public event Action? UserNameChanged;
+
+    public void SetUserName(string userName)
+    {
+        UserName = userName;
+        UserNameChanged?.Invoke();
+    }
+
+    public string UserAvatar { get; set; } = string.Empty;
+    public event Action? UserAvatarChanged;
+
+    public void SetUserAvatar(string userAvatar)
+    {
+        UserAvatar = userAvatar;
+        UserAvatarChanged?.Invoke();
+    }
 }
