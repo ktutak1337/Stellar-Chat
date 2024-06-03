b653eed419080f2ecfd7a35c378e550b40fe1ce1
ktutak1337 <ktutak1337@gmail.com>
2024-06-03 11:24:44 +0200

    Refactor(API::AssistantResponse): Change type from record to class for AssistantResponse in Shared.Contracts

--- src/Shared/StellarChat.Shared.Contracts/Assistants/AssistantResponse.cs
+++ src/Shared/StellarChat.Shared.Contracts/Assistants/AssistantResponse.cs
@@ -1,13 +1,15 @@
 ﻿namespace StellarChat.Shared.Contracts.Assistants;
 
-public record AssistantResponse(
-    Guid Id,
-    string Name,
-    string Metaprompt, 
-    string Description,
-    string AvatarUrl,
-    string DefaultModel,
-    string DefaultVoice,
-    bool IsDefault,
-    DateTimeOffset CreatedAt,
-    DateTimeOffset UpdatedAt);
+public class AssistantResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Metaprompt { get; set; } = string.Empty; 
+    public string Description { get; set; } = string.Empty;
+    public string AvatarUrl { get; set; } = string.Empty;
+    public string DefaultModel { get; set; } = string.Empty;
+    public string DefaultVoice { get; set; } = string.Empty; 
+    public bool IsDefault { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
+}
