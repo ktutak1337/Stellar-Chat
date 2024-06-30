665c01bfc0a5401f6c5c019e1de9a3cf9dfb3e76
ktutak1337 <ktutak1337@gmail.com>
2024-06-30 22:05:23 +0200

    Code(WEB::ApiResponse): Add ApiResponse model for structured HTTP response handling

---
+++ src/Client/StellarChat.Client.Web/Shared/Http/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace StellarChat.Client.Web.Shared.Http;
+
+public record ApiResponse(HttpResponseMessage HttpResponse, bool Succeeded, ErrorsResponse? Errors = null);
+
+public record ApiResponse<T>(T Value, HttpResponseMessage HttpResponse, bool Succeeded, ErrorsResponse? Errors = null) : ApiResponse(HttpResponse, Succeeded, Errors);
+
+public record ApiError(string Code, string Message, string? UserMessage);
+
+public record ErrorsResponse(ApiError[] Errors);
