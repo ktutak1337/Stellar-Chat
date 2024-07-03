dba919bcbbfca12047d6f74d30cce0f0a8a6241b
ktutak1337 <ktutak1337@gmail.com>
2024-07-03 05:23:15 +0200

    Refractor(WEB::Program): Code refractor and cleanup

---
+++ src/Client/StellarChat.Client.Web/Extensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using MudBlazor.Services;
+using StellarChat.Client.Web.Services.Actions;
+using StellarChat.Client.Web.Services.Assistants;
+using StellarChat.Client.Web.Services.Chat;
+using StellarChat.Client.Web.Services.Models;
+using StellarChat.Client.Web.Services.Settings;
+using StellarChat.Client.Web.Services.Storage;
+using StellarChat.Client.Web.Shared.Http;
+using StellarChat.Client.Web.State;
+
+namespace StellarChat.Client.Web;
+
+internal static class Extensions
+{
+    public static WebAssemblyHostBuilder AddServices(this WebAssemblyHostBuilder builder)
+    {
+        builder.Services.TryAddSingleton(TimeProvider.System);
+
+        builder.Services.AddScoped<IRestHttpClient, RestHttpClient>();
+        builder.Services.AddScoped<ChatState>();
+        builder.Services.AddScoped<IChatService, ChatService>();
+        builder.Services.AddScoped<IActionService, ActionService>();
+        builder.Services.AddScoped<IAssistantService, AssistantService>();
+        builder.Services.AddScoped<ISettingsService, SettingsService>();
+        builder.Services.AddScoped<IStorageService, StorageService>();
+        builder.Services.AddScoped<IAvailableModelsService, AvailableModelsService>();
+
+        builder.Services.AddMudServices();
+
+        return builder;
+    }
+}
--- src/Client/StellarChat.Client.Web/Program.cs
+++ src/Client/StellarChat.Client.Web/Program.cs
@@ -1,23 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using MudBlazor.Services;
 using StellarChat.Client.Web;
-using StellarChat.Client.Web.Services.Actions;
-using StellarChat.Client.Web.Services.Assistants;
-using StellarChat.Client.Web.Services.Chat;
-using StellarChat.Client.Web.Services.Models;
-using StellarChat.Client.Web.Services.Settings;
-using StellarChat.Client.Web.Services.Storage;
-using StellarChat.Client.Web.Shared.Http;
-using StellarChat.Client.Web.State;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.TryAddSingleton(TimeProvider.System);
 
 var apiUrl = builder.Configuration["api:url"];
 
@@ -26,14 +15,6 @@ builder.Services.AddHttpClient("WebAPI", client =>
     client.BaseAddress = new Uri(apiUrl!);
 });
 
-builder.Services.AddScoped<IRestHttpClient, RestHttpClient>();
-builder.Services.AddScoped<ChatState>();
-builder.Services.AddMudServices();
-builder.Services.AddScoped<IChatService, ChatService>();
-builder.Services.AddScoped<IActionService, ActionService>();
-builder.Services.AddScoped<IAssistantService, AssistantService>();
-builder.Services.AddScoped<ISettingsService, SettingsService>();
-builder.Services.AddScoped<IStorageService, StorageService>();
-builder.Services.AddScoped<IAvailableModelsService, AvailableModelsService>();
+builder.AddServices();
 
 await builder.Build().RunAsync();
