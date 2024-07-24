befeba35c7b7c9c3c4923a358c4cf5155197d458
ktutak1337 <ktutak1337@gmail.com>
2024-07-24 09:31:27 +0200

    Code(API::Mongo): Seed default Integrations (OpenAI, Ollama) if missing in AppSettingsDocument. Improved performance checks to avoid overwriting user settings

--- src/Server/StellarChat.Server.Api/DAL/Mongo/Seeders/AppSettingsSeeder.cs
+++ src/Server/StellarChat.Server.Api/DAL/Mongo/Seeders/AppSettingsSeeder.cs
@@ -11,11 +11,22 @@ public class AppSettingsSeeder(ILogger<AppSettingsSeeder> logger, TimeProvider c
         var cursor = await settingsCollection.FindAsync(FilterDefinition<AppSettingsDocument>.Empty);
         var documents = await cursor.ToListAsync();
 
-        if (documents.Any())
+        if (documents.Count == 0)
         {
+            await SeedNewSettingsAsync(settingsCollection);
             return;
         }
 
+        var appSettingsDocument = documents.First();
+        
+        if (appSettingsDocument.Integrations is null || appSettingsDocument.Integrations.Count == 0)
+        {
+            await UpdateSettingsWithIntegrationsAsync(settingsCollection, appSettingsDocument);
+        }
+    }
+
+    private async Task SeedNewSettingsAsync(IMongoCollection<AppSettingsDocument> settingsCollection)
+    {
         var now = _clock.GetLocalNow();
 
         var appSettingsDocument = new AppSettingsDocument
@@ -52,5 +63,31 @@ public class AppSettingsSeeder(ILogger<AppSettingsSeeder> logger, TimeProvider c
 
         _logger.LogInformation($"Added a settings document to the database with 'ID': {appSettingsDocument.Id}, and 'Key': {appSettingsDocument.Key}.");
         _logger.LogInformation("Finished seeding 'settings' collection.");
+    }
+
+    private async Task UpdateSettingsWithIntegrationsAsync(IMongoCollection<AppSettingsDocument> settingsCollection, AppSettingsDocument appSettingsDocument)
+    {
+        appSettingsDocument.Integrations = new List<IntegrationDocument>
+        {
+            new() 
+            {
+                Name = "OpenAI",
+                ApiKey = string.Empty,
+                Endpoint = string.Empty,
+                IsEnabled = true,
+            },
+            new()
+            {
+                Name = "Ollama",
+                ApiKey = string.Empty,
+                Endpoint = string.Empty,
+                IsEnabled = true,
+            },
+        };
+        appSettingsDocument.UpdatedAt = _clock.GetLocalNow();
+        _logger.LogInformation("Started updating 'settings' collection with integrations.");
+        await settingsCollection.ReplaceOneAsync(doc => doc.Id == appSettingsDocument.Id, appSettingsDocument);
+        _logger.LogInformation($"Updated the settings document in the database with 'ID': {appSettingsDocument.Id}.");
+        _logger.LogInformation("Finished updating 'settings' collection.");
     }
 }
