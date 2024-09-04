8cd65bd699ea868a0ffa69a2ef9ae521b7ca4f60
ktutak1337 <ktutak1337@gmail.com>
2024-09-04 11:08:07 +0200

    Code(WEB::AttachmentPreview): Move draft to separate component named AttachmentPreview

---
+++ src/Client/StellarChat.Client.Web/Models/Attachment.cs
@@ -0,0 +1,8 @@
+﻿namespace StellarChat.Client.Web.Models;
+
+public class Attachment
+{
+    public bool IsLoading { get; set; }
+    public string? Url { get; set; }
+    public bool IsHovering { get; set; }
+}
